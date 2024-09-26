318f359923ff844f41e4bb22c2e6392efca6cc81
StephaneEhu <niamianstephane@gmail.com>
2024-09-26 12:53:46 +0200

    Change consul to eureka

--- src/main/java/com/madisfinance/walletgateway/WalletGatewayApplication.java
+++ src/main/java/com/madisfinance/walletgateway/WalletGatewayApplication.java
@@ -1,6 +1,6 @@
 package com.madisfinance.walletgateway;
 
- import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springdoc.core.models.GroupedOpenApi;
@@ -10,32 +10,38 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
-import org.springframework.context.annotation.Bean;   
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
-@EnableDiscoveryClient 
-public class WalletGatewayApplication { 
+@EnableDiscoveryClient
+public class WalletGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WalletGatewayApplication.class, args);
 	}
 
-	@Autowired
-	RouteDefinitionLocator locator;
- 
+	private final RouteDefinitionLocator locator;
+	
+
+	public WalletGatewayApplication(RouteDefinitionLocator locator) {
+		this.locator = locator;
+	}
+
+
 	@Bean
-	 List<GroupedOpenApi> apis() {
-	   List<GroupedOpenApi> groups = new ArrayList<>();
-	   List<RouteDefinition> definitions = locator
-		  .getRouteDefinitions().collectList().block();
-	   assert definitions != null;
-	   definitions.stream().filter(routeDefinition -> routeDefinition
-		  .getId()
-		  .matches(".*-service"))
-		  .forEach(routeDefinition -> {
-			 String name = routeDefinition.getId()
-				.replace("-service", "");
-			 groups.add(GroupedOpenApi.builder()
-				.pathsToMatch("/" + name + "/**").group(name).build());
-		  });
-	   return groups;
+	List<GroupedOpenApi> apis() {
+		List<GroupedOpenApi> groups = new ArrayList<>();
+		List<RouteDefinition> definitions = locator
+				.getRouteDefinitions().collectList().block();
+		assert definitions != null;
+		definitions.stream().filter(routeDefinition -> routeDefinition
+				.getId()
+				.matches(".*-service"))
+				.forEach(routeDefinition -> {
+					String name = routeDefinition.getId()
+							.replace("-service", "");
+					groups.add(GroupedOpenApi.builder()
+							.pathsToMatch("/" + name + "/**").group(name).build());
+				});
+		return groups;
 	}
 }
